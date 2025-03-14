7c8fa5b86db23ecf498ced81c0ec37898cc57ecb
Joaco <joaquinnogallar@gmail.com>
2025-03-14 19:35:08 -0300

    feat: structure and db connectivity

---
+++ src/main/java/com/joaquinogallar/parrobackend/ParroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joaquinogallar.parrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaquinogallar/parrobackend/ParroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaquinogallar.parrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
